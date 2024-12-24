aa85783aac9a51912aefac4dc8be7facbfe01857
sachin-dashzer <sachin8287037611@gmail.com>
2024-12-24 13:58:19 +0530

    everything works

--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,12 @@
 import express, { urlencoded } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv'
-
-
-dotenv.config({ path: './.env' });
 
 
 const app = express()
 
 app.use(cors({
-    origin: 'process.env.CORS_ORIGIN', 
+    origin: 'https://topical-epaper.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true, 
   }));
