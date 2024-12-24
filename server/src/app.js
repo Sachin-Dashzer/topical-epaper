f4f3f47342ccfc7d05ee674cba6e9f08f8164f06
sachin-dashzer <sachin8287037611@gmail.com>
2024-12-24 13:12:37 +0530

    port changes

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
 
-dotenv.config({path: './.env'})
+
+dotenv.config({ path: './.env' });
 
 
 const app = express()
