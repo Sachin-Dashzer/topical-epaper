73de55997f722c39763183979e770326385b4093
sachin-dashzer <sachin8287037611@gmail.com>
2024-12-27 20:11:50 +0530

    some thing updated

--- server/src/app.js
+++ server/src/app.js
@@ -17,9 +17,9 @@ app.use(cors({
 
   app.use(cookieParser());
 
-app.use(urlencoded({extended: true , limit : "14kb"}))
-app.use(express.json())
-app.use(express.static("public"))
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.use(express.static("public"))
 
 
 import authRouter from './routes/authRoutes.js'
